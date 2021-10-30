256f7f0479d20d93eabc1c3132aa2e0434bfdcd0
Amit Gajurel <gajurelamit@gmail.com>
2021-10-30 02:48:49 -0500

    Question 1A complete: Read the notes carefully. Post processing needs to be done. Deflection values seems to match the reference

--- Project-1A/source/main.py
+++ Project-1A/source/main.py
@@ -3,6 +3,9 @@ import yaml
 import glob
 import os
 import numpy as np
+from utils.functions import stiffnessmatrix as sm
+import matplotlib.pyplot as plt
+
 
 # Make sure your working directory is same as the scripts locations
 path = os.path.dirname(__file__)
@@ -18,15 +21,15 @@ temp = open(files[0],"r")
 config = yaml.full_load(temp)
 
 # assigning values to the variables. Reading from the input.yml file
-su0 , k , gamma, e50, d0, t, l , E, yld ,nx, tol, yn, pn, J, BC = list(config.values())
+su0 , k , gamma, e50, d0, t, l , E, yld ,nx, tol, yn, pn, J, BC, delta_force, head_cond, tip_cond = list(config.values())
 
 # Calculation of derived quantities of the piles
 I = np.pi/64 * (d0**4 - (d0-2*t)**4)            # moment of inertia for circular hollow pile
 S = 2 * I/d0                                    # section modulus
-EI = E*I
+EI = E*I/(12**2)                                  # Changing the unit to lbs.ft2
 
 # reference deflection
-yc = 2.5 * e50 * d0
+y50 = 2.5 * e50 * d0 / 12                       # Unit from here onwards is ft.
 
 # Calcualtion of Pmax in the the P-y curve equation
 dx = l/nx
@@ -41,7 +44,7 @@ for i in np.linspace(1,num,num):
     i = int(i)                                  # indexing can only be done with int not float
     xi.append((i - 1)*dx)
     su.append(su0 + k * xi[i-1])
-    Np.append(3 + (gamma-9.81)/su[i-1]*xi[i-1] + J/d0*xi[i-1])
+    Np.append(3 + (gamma-62.4)/su[i-1]*xi[i-1] + J/(d0/12)*xi[i-1])  # all units in ft
     Pmax.append(Np[i-1] * su[i-1])
 
 # limiting the maximum value of Np as 9
@@ -51,6 +54,42 @@ for idx, val in enumerate(Np):
     else:
         Np[idx] = 9
 
-cvc = 2
+# Initializing intial displacement field with triangular displacement field with max of 4 in of displacement
+y = np.linspace(0.1,0.0,num)
+
+# initializing the P values for a given set of displacements based on the given API P-y curves
+P = np.zeros(num)
+
+# initializing ks - slope of P-y curve values for each and very node.
+ks = np.zeros(num)
+
+# difference between predicted from the inversion and the P-y curve
+y_diff = 1
+
+# Running while loop until the difference between y that statifies the boundary condition and the P-y curve is met
+loop_count = 0 
+
+while y_diff > tol:
+
+    # calculating P and ks values for each and every node given the displacement field
+    for idx,val in enumerate(y/y50):
+        if val < 8:
+            P[idx] = 0.5 * np.abs(val)**(1/3) * Pmax[idx]
+            ks[idx] = P[idx]/np.abs(val*y50)                                # Make sure that Ks is positive when iterating
+        else:
+            P[idx] = Pmax[idx]
+            ks[idx] = P[idx]/(val*y50)
+
+
+    [k, R] = sm(BC,num,dx,ks,delta_force, head_cond, tip_cond,EI)
+
+    y_solved = np.dot(np.linalg.inv(k),R)
+    y_diff = np.mean(np.abs(y-y_solved))
+    y = y_solved
+    loop_count+=1
+
+plt.plot(y,-1*np.linspace(1,num,num))
+plt.savefig("Temp.jpg")
+print("This is the end")
 
 
--- Project-1A/source/utils/functions.py
+++ Project-1A/source/utils/functions.py
@@ -2,15 +2,20 @@
 
 def stiffnessmatrix(BC,num,dx,ks,delta_Force,head_cond,tip_cond,EI):
     """
+    INPUTS
+
     BC              : Boundary Condition If BC = 0 (disp. controlled) or 1 (force controlled)
     num             : Number of nodes i.e. number of segments + 1
     dx              : Size of each element
-    ks              : Spring stiffness
+    ks              : Spring stiffness for all the internal points/nodes along the pile
     delta_force     : if BC = 0 provide the delta (in.) and if BC = 1 provide force (lbs.)
     head_cond       : is equal to 0 for free head and 1 for fixed head
     tip_cond        : is equal to 0 for zero moment & zero shear at bottom i.e. floating pile; 1 for zero displacement and zero slope i.e. pile in rock
     EI              : Flexural Stiffness
     
+    RETURNS
+    [k, R]          : Where k -> stiffness matrix and R -> Load Vector
+
     """
     import numpy as np
 
@@ -23,19 +28,19 @@ def stiffnessmatrix(BC,num,dx,ks,delta_Force,head_cond,tip_cond,EI):
         # Using 4th order beam bending problem with central difference 
         k[val,val-2] = 1
         k[val,val-1] = -4
-        k[val,val] = 6 + ks * dx**4 / EI# Spring stiffness term
+        k[val,val] = 6 + ks[val] * dx**4 / EI               # Spring stiffness term
         k[val,val+1] = -4
         k[val,val+2] = 1
 
     # First node is used for force or displacement control BC at the tip of the pile
     if BC == 0:
-        k[0,] = 0               # zero all
-        k[0,0] = 1              # for displacement control
+        k[0,] = 0                                           # zero all
+        k[0,0] = 1                                          # for displacement control
         R[0,0] = delta_Force    
     elif BC == 1:
         # Third order forward difference of shear force
         k[0,] = 0
-        k[0,0] = -1             # -ve sign is to account for the direction of force and direction of displacement
+        k[0,0] = -1                                         # -ve sign is to account for the direction of force and direction of displacement
         k[0,1] = 3
         k[0,2] = -3
         k[0,3] = 1
@@ -48,7 +53,7 @@ def stiffnessmatrix(BC,num,dx,ks,delta_Force,head_cond,tip_cond,EI):
         k[1,1] = -2
         k[1,2] = 1
         R[1,0] = 0          # Free head has moment zero 
-    if head_cond == 1:
+    elif head_cond == 1:
         # Fixed head -> Slope = 0 Using 1st order forward difference
         k[1,0] = 1
         k[1,1] = -1
@@ -68,7 +73,7 @@ def stiffnessmatrix(BC,num,dx,ks,delta_Force,head_cond,tip_cond,EI):
         k[num-2,num-3] = 3
         k[num-2,num-2] = -3
         k[num-2,num-1] = 1
-    if tip_cond == 1:
+    elif tip_cond == 1:
         # last row of node is used for type of boundary condition at the bottom of the pile
         # imposing displacement equal to zero at the tip/bottom of the pile
         k[num-1,num-1] = 1
