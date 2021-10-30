e7531a595b7a1f8e3f4ccbaad58d146da866a5a4
Amit Gajurel <gajurelamit@gmail.com>
2021-10-29 23:44:52 -0500

    completed the stiffness matrix function for the lateral load

--- Project-1A/source/utils/functions.py
+++ Project-1A/source/utils/functions.py
@@ -1,13 +1,14 @@
 # this script contains necessary function for the main scripts
 
-def stiffnessmatrix(BC = 0,num,dx,ks,delta_Force,head_cond,EI):
+def stiffnessmatrix(BC,num,dx,ks,delta_Force,head_cond,tip_cond,EI):
     """
     BC              : Boundary Condition If BC = 0 (disp. controlled) or 1 (force controlled)
     num             : Number of nodes i.e. number of segments + 1
     dx              : Size of each element
     ks              : Spring stiffness
     delta_force     : if BC = 0 provide the delta (in.) and if BC = 1 provide force (lbs.)
-    head_condition  : is equal to 0 for free head and 1 for fixed head
+    head_cond       : is equal to 0 for free head and 1 for fixed head
+    tip_cond        : is equal to 0 for zero moment & zero shear at bottom i.e. floating pile; 1 for zero displacement and zero slope i.e. pile in rock
     EI              : Flexural Stiffness
     
     """
@@ -22,11 +23,11 @@ def stiffnessmatrix(BC = 0,num,dx,ks,delta_Force,head_cond,EI):
         # Using 4th order beam bending problem with central difference 
         k[val,val-2] = 1
         k[val,val-1] = -4
-        k[val,val] = # Spring stiffness term
+        k[val,val] = 6 + ks * dx**4 / EI# Spring stiffness term
         k[val,val+1] = -4
         k[val,val+2] = 1
 
-    # First node is used for force or displacement control BC
+    # First node is used for force or displacement control BC at the tip of the pile
     if BC == 0:
         k[0,] = 0               # zero all
         k[0,0] = 1              # for displacement control
@@ -40,7 +41,7 @@ def stiffnessmatrix(BC = 0,num,dx,ks,delta_Force,head_cond,EI):
         k[0,3] = 1
         R[0,0] = delta_Force * dx**3 / EI
     
-    # Second row of nodes is used for type of boundary condition
+    # Second row of nodes is used for type of boundary condition at the top of the pile
     if head_cond == 0:
         # Free head -> Moment = 0 Using 2nd order forward difference 
         k[1,0] = 1
@@ -53,9 +54,30 @@ def stiffnessmatrix(BC = 0,num,dx,ks,delta_Force,head_cond,EI):
         k[1,1] = -1
         R[1,0] = 0          # Fixed head has slope 0
 
+    # last two row of node is used for type of boundary condition at the bottom of the pile
+    if tip_cond == 0:
+        # last row of node is used for type of boundary condition at the bottom of the pile
+        # imposing moment equal to zero at the tip/bottom of the pile
+        k[num-1,num-3] = 1
+        k[num-1,num-2] = -2
+        k[num-1,num-1] = 1
+        
+        # second to the last row of node is used for type of boundary condition at the bottom of the pile
+        # imposing zero shear at the bottom
+        k[num-2,num-4] = -1
+        k[num-2,num-3] = 3
+        k[num-2,num-2] = -3
+        k[num-2,num-1] = 1
+    if tip_cond == 1:
+        # last row of node is used for type of boundary condition at the bottom of the pile
+        # imposing displacement equal to zero at the tip/bottom of the pile
+        k[num-1,num-1] = 1
 
+        # second to last tow of node is used for slope is zero at the bottom
+        k[num-2,num-4] = 1
+        k[num-2,num-3] = -2
+        k[num-2,num-2] = 1
 
-
-    return(k)
+    return([k,R])
 
     
