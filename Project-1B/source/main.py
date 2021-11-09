338b4626680888dd34ba1d6dfab0bde35acbeafb
Amit Gajurel <gajurelamit@gmail.com>
2021-11-09 10:49:33 -0600

    Project 1b initial commit. Still the errors left to figure out

---
+++ Project-1B/source/main.py
@@ -0,0 +1,166 @@
+from sys import path
+import yaml
+import glob
+import os
+import numpy as np
+from utils.functions import stiffnessmatrix as sm
+from utils.functions import custom_plot 
+import matplotlib.pyplot as plt
+import csv
+from scipy.interpolate import interp1d
+
+
+# Make sure your working directory is same as the scripts locations
+path = os.path.dirname(__file__)
+
+# Changing the CWD to the current working directory
+os.chdir(path)
+
+# loading the YAML files in the the data directory
+files = glob.glob(pathname="../data/*.yml")
+temp = open(files[0],"r")
+
+# YAML loads the files a dictionary
+config = yaml.full_load(temp)
+
+# assigning values to the variables. Reading from the input.yml file
+su0 , k , gamma, e50, alpha,Nc,\
+    d0, t, l , E, yld, \
+        nx, tol,\
+        Qn, Z1n, tn, Z2n,\
+             BC, delta_force, head_cond, tip_cond, plug_cond = list(config.values())
+
+# Calculation of derived quantities of the piles
+A = np.pi/4 * (d0**2 - (d0-2*t)**2)                 # Crosssectional Area of the pile
+A0 = np.pi/4 * (d0**2)                              # Crossection area of the pile for plugged case
+EA = E*A                                            # Output units in pounds               
+
+
+# Initializing the value of Tmax in the the t-z curve equation along all the nodes
+dx = l/nx                                           # Total number of elements
+num = nx + 1                                        # Total number of nodes
+x = np.linspace(0,l,num)                            # Divides the "l" into "num" nodes starting from 0
+xi = []
+su = []
+tmax = []
+
+for idx,val in enumerate(x):                        # populating each node with su and tmax value
+    su.append(su0 + k * val)
+    tmax.append(alpha * su[idx] * np.pi * d0/12)    # all units in ft
+
+
+# Initializing intial displacement field with triangular displacement field with max of 4 in of displacement
+deltamin=0.002                                  # Initial minimum displacement at the bottom of the pile
+deltamax = 0.4                                 # Initial maximum displacement at the top of the pile
+z = np.linspace(deltamax,deltamin,num)
+
+# initializing the t values for calculating given set of displacements based on the given API t-z curves
+t = np.zeros(num)
+
+# initializing ks - slope of t-z curve values for each and every node.
+ks = np.zeros(num)
+
+# initialzing the Q value for q-z curve for each and every node
+kp = np.zeros(num)
+
+# difference between predicted from the inversion and the t-z curve
+z_diff = 1
+
+# Running while loop until the difference between y that statifies the boundary condition and the P-y curve is met
+loop_count = 0 
+
+while z_diff > tol:
+    # Working with the API t-z curves to fill up the matrix of ks
+    intp_tn = interp1d(x=Z2n,y=tn,kind='linear')(np.abs(z/(d0/12)))        # value of interpolated tn i.e. y-axis of t-z API curve
+     
+    for idx,val in enumerate(tmax):
+        tmob = intp_tn[idx] * tmax[idx]
+        ks[idx] =  (tmob * np.pi * d0/12) / z[idx]                     # Stiffness in terms of Force/Area
+       
+    # Working witht the API q-z curve
+    intp_qn = interp1d(x=Z1n, y=Qn,kind='linear')(np.abs(z[num-1]/(d0/12)))
+    qmax = su[num-1] * Nc * A0
+    qmob = intp_qn * qmax
+    kp[num-1]= qmob / z[num-1]
+
+
+    [k, R] = sm(num,dx,ks,z,kp,delta_force, EA, BC,tip_cond, plug_cond)
+
+    z_solved = np.dot(np.linalg.inv(k),R)
+    z_diff = np.max(np.abs(z-z_solved))
+    z = z_solved
+    loop_count+=1
+    if loop_count > 1000:
+        break
+
+### Post Processing for plotting and exporting the results
+
+# Depth Vs Displacement
+depth = np.linspace(0,l,num)                                             # Creating a plotting value along the depth
+displacement = [x for x in z]*12                             # Changing the list of one element arrary to float and multiplying to get disp. in in.                                                    # Changing the ft to in. to display the displacement
+plt = custom_plot(x=z*12,y=depth,xlabel="Displacement(in.)",
+                  ylabel="Depth (ft)",title="Displacement Vs Depth ")   
+plt.savefig('../output/DisplvsDepth.jpg')                               # Saving figure outputs
+
+# Writing a CSV file that was used to plot the above figure
+with open('../output/DisplvsDepth.csv','w') as csvfile:
+    writer = csv.writer(csvfile,delimiter=',')
+    writer.writerow(['Depth (Ft)', 'Displacement (in)'])
+    writer.writerows(zip(depth,displacement))
+
+
+
+BM = np.zeros(num)
+SF = np.zeros(num)
+SR = np.zeros(num)
+
+# Depth, Shear Force, Soil resistance Vs Bending moment
+for idx,val in enumerate(np.arange(0,num-4)):
+    # using forward difference for 0 to num-4. i.e. leaving last 4 nodes
+    BM[val] = EI * (y[val]-2*y[val+1]+y[val+2])/dx**2
+    SF[val] = EI * (-y[val]+3*y[val+1]-3*y[val+2]+y[val+3])/dx**3
+    SR[val] = EI * (y[val]-4*y[val+1]+6*y[val+2]-4*y[val+3]+y[val+4])/dx**4
+
+for idx,val in enumerate(np.arange(num-4,num)):
+    # using backward difference for num-4 to num. i.e. including last 4 nodes
+    BM[val] = EI * (y[val]-2*y[val-1]+y[val-2])/dx**2
+    SF[val] = EI * (y[val]-3*y[val-1]+3*y[val-2]-y[val-3])/dx**3
+    SR[val] = EI * (y[val]-4*y[val-1]+6*y[val-2]-4*y[val-3]+y[val-4])/dx**4
+
+depth = np.linspace(0,l,num)                                             # Creating a plotting value along the depth
+plt = custom_plot(x=BM,y=depth,xlabel="Moment(ft-lb)",
+                  ylabel="Depth (ft)",title="Moment Vs Depth ")   
+plt.savefig('../output/MomentvsDepth.jpg')                               # Saving figure outputs
+
+# Writing a CSV file that was used to plot the above figure
+with open('../output/MomentvsDepth.csv','w') as csvfile:
+    writer = csv.writer(csvfile,delimiter=',')
+    writer.writerow(['Depth (Ft)', 'Moment (Ft-lb)'])
+    writer.writerows(zip(depth,BM))
+
+
+plt = custom_plot(x=SF,y=depth,xlabel="Shear(lbs.)",
+                  ylabel="Depth (ft)",title="Shear Vs Depth ")   
+plt.savefig('../output/ShearvsDepth.jpg')                               # Saving figure outputs
+
+# Writing a CSV file that was used to plot the above figure
+with open('../output/ShearvsDepth.csv','w') as csvfile:
+    writer = csv.writer(csvfile,delimiter=',')
+    writer.writerow(['Depth (Ft)', 'Shear (lbs.)'])
+    writer.writerows(zip(depth,SF))
+
+
+plt = custom_plot(x=SR*-1,y=depth,xlabel="Soil Resistance(lbs/ft.ft)",
+                  ylabel="Depth (ft)",title="Soil Resistance Vs Depth ")   
+plt.savefig('../output/SRvsDepth.jpg')                               # Saving figure outputs
+
+# Writing a CSV file that was used to plot the above figure
+with open('../output/SRvsDepth.csv','w') as csvfile:
+    writer = csv.writer(csvfile,delimiter=',')
+    writer.writerow(['Depth (Ft)', 'Shear (lbs.)'])
+    writer.writerows(zip(depth,SR*-1))
+
+print("This is the end")
+
+
+
---
+++ Project-1B/source/utils/functions.py
@@ -0,0 +1,102 @@
+# this script contains necessary function for the main scripts
+
+def stiffnessmatrix(num,dx ,ks,kp,z,delta_Force ,EA ,BC = 0,tip_cond=0, plug_cond = 0):
+    """
+    INPUTS
+
+    BC              : Boundary Condition If BC = 0 (disp. controlled) or 1 (force controlled)
+    num             : Number of nodes i.e. number of segments + 1
+    dx              : Size of each element
+    ks              : Spring stiffness for side friction at all the internal points/nodes along the pile
+    kp              : Spring stiffness for the end bearing for the last node of the pile
+    delta_force     : if BC = 0 provide the delta (in.) and if BC = 1 provide force (lbs.)
+    tip_cond        : is equal to 0 for displacement possible at bottom ; 1 for zero displacement i.e. pile in rock
+    plug_cond       : is equal to 0 for plugged pile
+    z               : axial displacement profile
+    EA              : Axial Stiffness
+    
+    RETURNS
+    [k, R]          : Where k -> stiffness matrix and R -> Load Vector
+
+    """
+    import numpy as np
+
+    # Creating a numpy arrary of zeros with size num x num
+    k = np.zeros((num,num))
+    R = np.zeros((num,1))
+
+    # Central Nodes
+    for idx,val in enumerate(np.arange(1,num-2)):
+        # Using 2nd order finite difference approximation for axial load tranfer equation i.e. EA d2z/dx2 -ksz=0 
+        k[val,val-1] = 1
+        k[val,val] = -1 * (2 + (ks[val] * dx**2)/ EA)                # Spring axial stiffness term
+        k[val,val+1] = 1
+
+    # First node is used for force or displacement control BC at the tip of the pile
+    if BC == 0:
+        k[0,] = 0                                           # zero all
+        k[0,0] = 1                                          # for displacement control
+        R[0] = delta_Force/12                             # Changing the unit from input in. to ft.   
+    elif BC == 1:
+        # TODO: Need to check
+        k[0,] = 0
+        k[1,1] = 1
+        k[1,0] = -1
+        R[1] = delta_Force * dx / EA                        # Changing force to displacement
+        
+    # last row of node is used for type of boundary condition at the bottom of the pile
+    
+    if plug_cond == 0:
+        # last row of node is used for type of boundary condition at the bottom of the pile
+        # imposing plugged at the bottom i.e. there is resistance from the soil at the bottom of the pile
+        k[num-1,num-1] = 1
+        k[num-1,num-2] = -1
+        R[num-1] = -kp[num-1] * z[num-1] * dx / EA
+        
+    elif plug_cond == 1:
+        # last row of node is used for type of boundary condition at the bottom of the pile
+        # imposing no force at the boundary
+        # TODO: Check
+        pass
+ 
+
+    return([k,R])
+
+def custom_plot(x,y,xlabel,ylabel,title):
+    """The function takes makes a pretty plot with labels for piles
+
+    Args:
+        x (array/list): List or array to plot in X-axis
+        y (array/list): List or array to plot in Y-axis
+        xlabel (string): String for Xlabel
+        ylabel (string): Strimg fpr Ylabel
+        title (string) : String for title
+    Output:
+        Plot object
+    """
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+    import seaborn as sb
+    import pandas as pd
+    import numpy as np
+
+    fig, axs = plt.subplots(dpi=600)
+    fig.set_size_inches(6,6)
+    fig.tight_layout(pad=4)
+
+    axs.xaxis.set_tick_params(which='major', size=5, width=0.7, direction='in', top='on')
+    axs.xaxis.set_tick_params(which='minor', size=2.5, width=0.35, direction='in', top='on') 
+
+    axs.yaxis.set_tick_params(which='major', size=5, width=0.7, direction='in', right='on')
+    axs.yaxis.set_tick_params(which='minor', size=2.5, width=0.35, direction='in', right='on')
+
+    axs.plot(x,y,'--')
+    axs.invert_yaxis()
+
+    axs.set_title(title,fontsize=10,fontweight='bold')
+    axs.set_xlabel(xlabel)
+    axs.set_ylabel(ylabel)
+    axs.grid(linewidth='0.25')
+    
+    plt.ioff()
+    return(fig)
